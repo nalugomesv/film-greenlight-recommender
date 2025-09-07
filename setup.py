beeb06bf596d12ce8f298010a9293f9766b74511
nalugomesv <165437332+nalugomesv@users.noreply.github.com>
2025-09-07 15:30:49 -0300

    chore(repo): initialize project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data-science challenge on film data: EDA and basic ML to guide which movie to greenlight, with hypotheses, overview-based insights, and IMDB rating prediction.',
+    author='Ana Luiza Gomes Vieira',
+    license='MIT',
+)
